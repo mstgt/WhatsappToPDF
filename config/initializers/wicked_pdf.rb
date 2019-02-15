d3819583be21ab0592492a667578724a400f8fbd
Mustafa <mustafo@gmail.com>
2019-02-15 15:59:10 -0500

    wkhtmltopdflinux

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,30 +8,30 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
- WickedPdf.config = {
+ #WickedPdf.config = {
         # Path to the wkhtmltopdf executable: This usually isn't needed if using
         # one of the wkhtmltopdf-binary family of gems.
         # exe_path: '/usr/local/bin/wkhtmltopdf',
         #   or
         # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-   exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+   #exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
         #exe_path: 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
         # Layout file to be used for all PDFs
         # (but can be overridden in `render :pdf` calls)
         # layout: 'pdf.html',
- }
+ #}
 
 
-# if Rails.env.production?
-#   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-# else
-#   # Linux (check your processor for Intel x86 or AMD x64)
-#   # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
-#   # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
-#   # OS X
-#   #wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-0.9.9-OS-X.i386"            
-#   # Windows
-#   wkhtmltopdf_path = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-# end
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtml_production.deb"
+else
+  # Linux (check your processor for Intel x86 or AMD x64)
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
+  # OS X
+  #wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-0.9.9-OS-X.i386"            
+  # Windows
+  wkhtmltopdf_path = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+end
 
- #WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+ WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
